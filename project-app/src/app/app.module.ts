9564089616e5ed19317d430d0810310d80aab3de
Unknown <joaovictor_mendes@hotmail.com>
2017-03-07 13:35:36 -0300

    Project Base

--- project-app/src/app/app.module.ts
+++ project-app/src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppRoutingModule }     from './app-routing.module';
+import { AppComponent }         from './controller/app.component';
+import { InMemoryDataService }  from './model/in-memory-data.service';
+import { DashboardComponent }   from './controller/dashboard.component';
+import { HeroesComponent }      from './controller/heroes.component';
+import { HeroDetailComponent }  from './controller/hero-detail.component';
+import { HeroService }          from './controller/hero.service';
+import { HeroSearchComponent }  from './controller/hero-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        AppRoutingModule
+    ],
+    declarations: [ 
+        AppComponent,
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        HeroSearchComponent
+    ],
+    providers: [ HeroService ],
+    bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
